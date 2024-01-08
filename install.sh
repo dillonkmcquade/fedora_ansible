08e4296737f4105051704f92aba38669fea22cc8
Dillon McQuade <dillonkmcquade@gmail.com>
2024-01-08 17:08:37 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,44 +1,48 @@
 #!/bin/sh
 # This script is for automating installation of programs and repositories for Fedora on a fresh install.
 
-read -r "Press y to start: " yesno
-if [ "$yesno" != "y" ]
-then
-    echo 'Exiting program...'
-    sleep 1
-    exit
+read -p "Have you created an ssh key and uploaded it to github? [y/N]: " hasKey
+if [ $hasKey != "y" ]; then
+    echo "Exiting, configure ssh with github first."
+    exit 1
 fi
 
-read -r "Update Repos? [y/N]:" starrt
-if [ "$starrt" = "y" ]
-then
-    sudo dnf update -y
-    echo 'Update Successful'
-    sleep 2
-else
-    echo 'Not updating repos.'
-    sleep 2
-fi
+sudo dnf install -y git
 
 echo "Adding third-party repositories"
-echo 'Adding RPM fusion and proton repos'
-sudo dnf install -y -q 'https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm' >/dev/null 2>&1
+echo "Adding RPM fusion and proton repos"
+sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+sudo dnf install -y "https://repo.protonvpn.com/fedora-$(rpm -E %fedora)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.1-2.noarch.rpm"
 
 echo 'Adding Brave repository'
-sudo dnf config-manager -y --add-repo 'https://brave-browser-rpm-release.s3.brave.com/x86_64/' >/dev/null 2>&1
-sudo rpm --import 'https://brave-browser-rpm-release.s3.brave.com/brave-core.asc' >/dev/null 2>&1
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager -y --add-repo "https://brave-browser-rpm-release.s3.brave.com/x86_64/"
+sudo rpm --import "https://brave-browser-rpm-release.s3.brave.com/brave-core.asc"
+
+echo "Adding docker repository"
+sudo dnf config-manager --add-repo "https://download.docker.com/linux/fedora/docker-ce.repo"
+
+echo "Enabling lazygit copr repo"
+sudo dnf copr enable atim/lazygit -y
 
 echo 'Downloading core programs'
-while IFS= read -r line;do
-    echo "Installing $line"
-    sudo dnf install -y -q "$line" >/dev/null 2>&1 && echo "$line installed successfully." || echo "Failed to install $line"
-done < "programs.txt"
-echo "Core programs installed."
+xargs --arg-file=./programs.txt --delim='\n' sudo dnf install -y
 
-echo "Copying configurations"
-cp -r "$(pwd)/tmux" "$XDG_CONFIG_HOME/"
-cp -r "$(pwd)/nvim" "$XDG_CONFIG_HOME/"
-cp -r "$(pwd)/lf" "$XDG_CONFIG_HOME/"
-mkdir "$XDG_CONFIG_HOME/zsh"
-cp -r "$(pwd)/zsh" "$XDG_CONFIG_HOME/"
-ln -sf "$XDG_CONFIG_HOME/zsh/.zshrc" "/home/$USER/.zshrc"
+echo "Copying configurations to their new home"
+cp -r tmux "/home/$USER/.config/"
+cp -r lf "/home/$USER/.config/"
+mkdir "/home/$USER/.config/zsh"
+cp .zshrc "/home/$USER/.config/zsh"
+ln -sf "/home/$USER/.config/zsh/.zshrc" "/home/$USER/.zshrc"
+
+echo "installing pyenv"
+curl "https://pyenv.run" | bash
+
+echo "cloning nvim"
+git clone "git@github.com:dillonkmcquade/nvim.git" "/home/$USER/.config/nvim"
+
+alternatives --config java
+
+echo "installing ohmyzsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
