e58ced2438ef9f574449b80b57172dcaca5316e3
Dillon McQuade <dillonkmcquade@gmail.com>
2024-01-09 10:00:04 -0500

    improve install script

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 # This script is for automating installation of programs and repositories for Fedora on a fresh install.
 
-read -p "Have you created an ssh key and uploaded it to github? [y/N]: " hasKey
-if [ $hasKey != "y" ]; then
-    echo "Exiting, configure ssh with github first."
-    exit 1
+# verify that ssh is enabled with github
+ssh -T git@github.com || { echo 'ssh not setup with github'; exit 1 }
+
+if ! command -v git
+then
+    sudo dnf install -y git
 fi
 
-sudo dnf install -y git
-
 echo "Adding third-party repositories"
-echo "Adding RPM fusion and proton repos"
-sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-sudo dnf install -y "https://repo.protonvpn.com/fedora-$(rpm -E %fedora)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.1-2.noarch.rpm"
+sudo dnf install -y \
+    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://repo.protonvpn.com/fedora-$(rpm -E %fedora)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.1-2.noarch.rpm" \
+    dnf-plugins-core
 
 echo 'Adding Brave repository'
-sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager -y --add-repo "https://brave-browser-rpm-release.s3.brave.com/x86_64/"
 sudo rpm --import "https://brave-browser-rpm-release.s3.brave.com/brave-core.asc"
 
@@ -30,17 +30,25 @@ echo 'Downloading core programs'
 xargs --arg-file=./programs.txt --delim='\n' sudo dnf install -y
 
 echo "Copying configurations to their new home"
-cp -r tmux "/home/$USER/.config/"
-cp -r lf "/home/$USER/.config/"
-mkdir "/home/$USER/.config/zsh"
-cp .zshrc "/home/$USER/.config/zsh"
-ln -sf "/home/$USER/.config/zsh/.zshrc" "/home/$USER/.zshrc"
+cp ./.zshrc "/home/$USER/.zshrc"
+
+if ! command -v parallel
+then
+    sudo dnf install -y parallel
+fi
+
+parallel git clone ::: \
+    "git@github.com:dillonkmcquade/nvim.git" \
+    "git@github.com:dillonkmcquade/tmux.git" \
+    "git@github.com:dillonkmcquade/lf.git" \
+    ::: \
+    "/home/$USER/.config/nvim" \
+    "/home/$USER/.config/tmux" \
+    "/home/$USER/.config/lf"
+
 
 echo "installing pyenv"
 curl "https://pyenv.run" | bash
-
-echo "cloning nvim"
-git clone "git@github.com:dillonkmcquade/nvim.git" "/home/$USER/.config/nvim"
 
 alternatives --config java
 
